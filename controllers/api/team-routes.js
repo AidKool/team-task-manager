516a39b6d89dedf147dbaf093cddd50fddb5b963
jordi <jordi.lanau@yahoo.com>
2022-02-19 22:26:15 +0000

    #11 Add route to get Teams and Tasks by Team ID

--- controllers/api/team-routes.js
+++ controllers/api/team-routes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { QueryTypes } = require('sequelize');
+const sequelize = require('../../config/connection');
 const { Team, Task, User } = require('../../models');
 
 router.get('/', async (req, res) => {
@@ -37,6 +39,31 @@ router.get('/:id', async (req, res) => {
     if (!teamData) {
       return res.status(404).json({ message: 'Team not found' });
     }
+    return res.status(200).json(teamData);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.get('/:id/tasks', async (req, res) => {
+  try {
+    const rawData = await sequelize.query(
+      'SELECT team.id, project_id, name, task.task_title FROM team LEFT JOIN user ON team.id = user.team_id LEFT JOIN task ON user.id = task.user_id WHERE team.id = :id',
+      { type: QueryTypes.SELECT, replacements: { id: req.params.id } }
+    );
+    if (rawData.length === 0) {
+      return res.status(404).json({ message: 'Team not found' });
+    }
+    const { id, project_id, name } = rawData[0];
+    const tasks = rawData
+      .filter((item) => item.task_title !== null)
+      .map((item) => item.task_title);
+    const teamData = {
+      id,
+      project_id,
+      name,
+      tasks,
+    };
     return res.status(200).json(teamData);
   } catch (error) {
     return res.status(500).json(error);
