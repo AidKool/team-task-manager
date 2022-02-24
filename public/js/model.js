5dc86140a338c388dfadb1234ae967a06c0ca8a4
David Davies <David_Davies@mail.com>
2022-02-24 19:07:09 +0000

    added a simple code to remove the class to show the modal and then add it to make it dissappear

---
+++ public/js/model.js
@@ -0,0 +1,20 @@
+
+function openModalProj() {
+  var element = document.getElementById("modalCreateProject");
+  element.classList.remove("modal");
+}
+
+function closeModalProj() {
+  var element = document.getElementById("modalCreateProject");
+  element.classList.add("modal");
+}
+
+function openModalTask() {
+  var element = document.getElementById("modalCreateTask");
+  element.classList.remove("modal");
+}
+
+function closeModalTask() {
+  var element = document.getElementById("modalCreateTask");
+  element.classList.add("modal");
+}
