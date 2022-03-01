30e8cf41b6ccf6dbf5d94b3e3d7be93c68915dd0
pfansiW <fancyfreelance76@gmail.com>
2022-03-01 04:12:37 +0000

    #46 completed the project-seeds file with project-deadline values

--- seeds/project-seeds.js
+++ seeds/project-seeds.js
@@ -3,14 +3,17 @@ const { Project } = require('../models');
 const projectData = [
   {
     title: 'Team Task Manager',
+    project_deadline: new Date('2022/05/01'),
     team_id: 1,
   },
   {
     title: 'Bitcoin tracker',
+    project_deadline: new Date('2022/06/01'),
     team_id: 2,
   },
   {
     title: 'Sports Bets',
+    project_deadline: new Date('2022/07/01'),
     team_id: 3,
   },
 ];
