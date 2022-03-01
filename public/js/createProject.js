674318e4bb13fe30833003807998483690fd0c0f
pfansiW <fancyfreelance76@gmail.com>
2022-03-01 04:05:43 +0000

    #46 created createProject.Js file function that will handle the modalCreateProj.handlebars data to the database

--- public/js/createProject.js
+++ public/js/createProject.js
@@ -4,4 +4,22 @@ const createProjectHandler = async (event) => {
   const projectDeadline = document
     .querySelector('#project-deadline')
     .value.trim();
+
+  if (projectTitle && projectDeadline) {
+    const response = await fetch('/api/projects', {
+      method: 'POST',
+      body: JSON.stringify({ projectTitle, projectDeadline }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      document.location.replace('/managerPg');
+      alert('Project created succesfully!');
+    } else {
+      alert(response.statusText);
+    }
+  }
 };
+
+document
+  .getElementById('#create-project-btn')
+  .addEventListener('click', createProjectHandler);
