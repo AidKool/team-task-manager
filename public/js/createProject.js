7d27903214de817c93fa59e41b02f6138e255fe1
jordi <jordi.lanau@yahoo.com>
2022-03-01 19:13:38 +0000

    #46 Add ID attribute to select options on create project

--- public/js/createProject.js
+++ public/js/createProject.js
@@ -4,12 +4,18 @@ const createProjectHandler = async (event) => {
   const projectDeadline = document
     .querySelector('#project-deadline')
     .value.trim();
-  const TeamSelected = document.querySelector('#team-list').value.trim();
+  const teamElement = document.querySelector('#assign-team');
+  const teamID = teamElement.options[teamElement.selectedIndex].dataset.id;
+  console.log(teamID.options[teamID.selectedIndex]);
 
-  if (projectTitle && projectDeadline && TeamSelected) {
+  if (projectTitle && projectDeadline && teamID) {
     const response = await fetch('/api/projects', {
       method: 'POST',
-      body: JSON.stringify({ projectTitle, projectDeadline, TeamSelected }),
+      body: JSON.stringify({
+        projectTitle,
+        projectDeadline,
+        TeamSelected: teamID,
+      }),
       headers: { 'Content-Type': 'application/json' },
     });
     if (response.ok) {
