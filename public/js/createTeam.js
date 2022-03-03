aac793148a91ee06d5b3dba11c80ebbbcf788212
jordi <jordi.lanau@yahoo.com>
2022-03-03 11:04:13 +0000

    #57 Add functionality to assign Employees to Teams

--- public/js/createTeam.js
+++ public/js/createTeam.js
@@ -1,4 +1,5 @@
 const createTeamForm = document.querySelector('.create-team');
+const assignEmployeeTeamForm = document.querySelector('.assign-employee-team');
 
 async function createTeamFormHandler(event) {
   event.preventDefault();
@@ -19,4 +20,31 @@ async function createTeamFormHandler(event) {
   }
 }
 
+async function assignEmployeeTeamFormHandler(event) {
+  event.preventDefault();
+
+  const teamElement = document.querySelector('.select-team');
+  const teamID = teamElement.options[teamElement.selectedIndex].dataset.id;
+  const employeeElement = document.querySelector('.select-employee');
+  const employeeID =
+    employeeElement.options[employeeElement.selectedIndex].dataset.id;
+
+  if (teamID && employeeID) {
+    const response = await fetch(`/api/users/${employeeID}`, {
+      method: 'PATCH',
+      body: JSON.stringify({ team_id: teamID }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
 createTeamForm.addEventListener('submit', createTeamFormHandler);
+assignEmployeeTeamForm.addEventListener(
+  'submit',
+  assignEmployeeTeamFormHandler
+);
